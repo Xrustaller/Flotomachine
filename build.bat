ef838172cf9b86fe62b19cb656d39515d6453d30
Xrustaller <stalker0077@gmail.com>
2023-02-17 04:00:10 +0300

    Фикс БД и скрипта

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
-dotnet publish .\Flotomachine\Flotomachine.csproj /t:CreateDeb /p:RuntimeIdentifier=linux-arm /p:SelfContained=true /p:TargetFramework=net7.0 /p:Configuration=Release /p:PublishProfile=Properties\PublishProfiles\RaspberryNet.pubxml
-
+dotnet publish /t:CreateDeb /p:RuntimeIdentifier=linux-arm /p:SelfContained=true /p:TargetFramework=net7.0 /p:Configuration=Release
 pause
